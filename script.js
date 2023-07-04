329bc06bd0b75e0950da0d7db6cf92496a28dac6
jbshryne <jbshryne@gmail.com>
2023-07-03 21:49:42 -0400

    implemented jQuery for ajax call, attached to submit button of form

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-$.ajax({
-  url: "https://api.magicthegathering.io/v1/cards?name=nissa&types=planeswalker",
-  success: function (data) {
-    // Handle data
-    console.log(data);
-  },
-  error: function (xhr, status, error) {
-    // Handle errors
-    console.error(error);
-  },
-});
+const $allCardsBtn = $("allCardsBtn");
+const $form = $("form");
+
+console.log($allCardsBtn);
+
+const searchFnc = (e) => {
+  e.preventDefault();
+
+  $.getJSON(
+    "https://api.magicthegathering.io/v1/cards",
+    function (data, textStatus, jqxhr) {
+      console.log(data);
+
+      // const pageSize = jqxhr.getResponseHeader('Page-Size');
+      // const count = jqxhr.getResponseHeader('Count');
+      // const totalCount = jqxhr.getResponseHeader('Total-Count');
+    }
+  ).fail(function (jqxhr, textStatus, error) {
+    console.error("Error:", error);
+  });
+};
+
+$form.on("submit", searchFnc);
